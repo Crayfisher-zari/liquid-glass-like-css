ad034b7f72a419e5a3df2c6a92078d0c5676c166
Nishihara Tsubasa <nishihara.tsubasa77@gmail.com>
2025-06-11 17:19:51 +0900

    デプロイ設定

---
+++ README.md
@@ -0,0 +1,1 @@
+# Liquid Glass風CSS
--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,40 @@
 import { defineConfig } from "vite";
+import { globSync } from "glob";
+import { relative, extname } from "path";
+import { fileURLToPath } from "node:url";
 import "dotenv/config";
 
-export default defineConfig({
-  base: process.env?.BASE_PATH ?? "/",
-  root: "./src/",
-  build: {
-    lib: {
-      entry: ["src/main.css"],
-      name: "style",
-    },
-    cssCodeSplit: true,
-    copyPublicDir: false,
+const libBuidSettings = {
+  lib: {
+    entry: ["src/main.css"],
+    name: "style",
   },
-});
+  cssCodeSplit: true,
+  copyPublicDir: false,
+};
+
+const docBuildSettings = {
+  outDir: "../docs",
+  rollupOptions: {
+    input: {
+      ...Object.fromEntries(
+        globSync("src/**/*.html").map((file) => [
+          relative("src", file.slice(0, file.length - extname(file).length)),
+          fileURLToPath(new URL(file, import.meta.url)),
+        ]),
+      ),
+    },
+    output: {
+      chunkFileNames: "assets/[name].js",
+      assetFileNames: "assets/[name][extname]",
+    },
+  },
+  target: "esnext",
+  assetsInlineLimit: 0,
+}
+
+export default defineConfig(({ mode }) => ({
+  base: process.env?.BASE_PATH ?? "/",
+  root: "src",
+  build: mode === "doc" ? docBuildSettings : libBuidSettings,
+}));
