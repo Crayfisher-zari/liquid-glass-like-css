220cb4593255bf8c2e06293adcac136d9d5de4ed
Nishihara Tsubasa <nishihara.tsubasa77@gmail.com>
2025-06-11 16:59:55 +0900

    ファイルの位置を調整

--- vite.config.ts
+++ vite.config.ts
@@ -3,11 +3,13 @@ import "dotenv/config";
 
 export default defineConfig({
   base: process.env?.BASE_PATH ?? "/",
+  root: "./src/",
   build: {
     lib: {
       entry: ["src/main.css"],
       name: "style",
     },
     cssCodeSplit: true,
+    copyPublicDir: false,
   },
 });
